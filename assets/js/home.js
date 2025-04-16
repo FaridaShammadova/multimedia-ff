2b0b8c86e6698d59001435b0bc9d3d49f3c10c38
Farida Shammadova <semmedovaferide017@gmail.com>
2025-04-16 14:35:49 +0400

    home updated, swiper and banner created

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,3 +2,14 @@ import { getData, getNewData } from "./services.js";
 
 getData();
 getNewData();
+
+
+new Swiper('.swiper', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
